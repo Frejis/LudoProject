222fdd9ad3a1dc9b488bbb28ce3ef4cf20da8b86
Frejis <93258034+Frejis@users.noreply.github.com>
2022-07-08 16:16:19 +0200

    Updates stuff

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author Carl-Emil Heiselberg Andersen, Dagmar Fischer Stephansen
  * @version 08/07/2022
@@ -5,8 +7,27 @@
 
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Shieeet");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter expression:");
+        String expression = myObj.next();
+//
+//        switch (exp) {
+//            case (expression.contains("%")):
+//                System.out.println("");
+//                break;
+//
+//        }
+        if (expression.contains("*")) {
+            int exps = expression.indexOf("*");
+            char[] leftExp = [];
+            for (int i = 0; i < exps; i++) {
+
+            }
+
+        }
     }
+
 
 }
